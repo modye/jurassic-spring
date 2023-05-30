ae9647cc804590f64618002ea9d258b512cdb50e
Maxime Odye <maxime.odye.ep@whoz.com>
2023-05-30 22:33:49 +0200

    feat(lister): dinos lister

---
+++ src/main/java/bzh/jurassic/spring/application/DinosLister.java
@@ -0,0 +1,10 @@
+package bzh.jurassic.spring.application;
+
+import java.util.List;
+
+public class DinosLister {
+
+    public List<String> findAllDinos() {
+        return List.of("T-Rex", "Velociraptor", "Poule");
+    }
+}
--- src/main/java/bzh/jurassic/spring/interfaces/JurassicController.java
+++ src/main/java/bzh/jurassic/spring/interfaces/JurassicController.java
@@ -1,16 +1,29 @@
 package bzh.jurassic.spring.interfaces;
 
+import bzh.jurassic.spring.application.DinosLister;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping(path = "/api/jurassic")
 public class JurassicController {
 
-    @GetMapping
+    // FIXME not so sure about that, found the answer on ChatGPT
+    @Autowired
+    private DinosLister dinosLister;
+
+    @GetMapping("/graouuu")
     public ResponseEntity<String> graouuu() {
         return ResponseEntity.ok("\uD83E\uDD96 Graaaaaouuuuu le BreizhCamp !! \uD83E\uDD96");
+    }
+
+    @GetMapping
+    public ResponseEntity<List<String>> dinos() {
+        return ResponseEntity.ok(dinosLister.findAllDinos());
     }
 }
