da8f77a29df7507e29b58a45b88526f0d0f44756
Maxime Odye <maxime.odye.ep@whoz.com>
2023-06-05 21:44:52 +0200

    feat(context): debug application context step

--- src/main/java/bzh/jurassic/spring/JurassicApplication.java
+++ src/main/java/bzh/jurassic/spring/JurassicApplication.java
@@ -2,12 +2,15 @@ package bzh.jurassic.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class JurassicApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(JurassicApplication.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(JurassicApplication.class, args);
+
+		System.out.println("You should add a breakpoint on this line :)");
 	}
 
 }
