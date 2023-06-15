7c9c04ef44db87ca2fdf90c127b53fc52e15be19
Maxime Odye <maxime.odye.ep@whoz.com>
2023-06-15 13:14:20 +0200

    feat(context): debug application context step

--- src/main/java/bzh/jurassic/spring/JurassicApplication.java
+++ src/main/java/bzh/jurassic/spring/JurassicApplication.java
@@ -1,5 +1,6 @@
 package bzh.jurassic.spring;
 
+import bzh.jurassic.spring.application.DinosLister;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -11,6 +12,12 @@ public class JurassicApplication {
 		ConfigurableApplicationContext context = SpringApplication.run(JurassicApplication.class, args);
 
 		System.out.println("You should add a breakpoint on this line :)");
+
+		DinosLister dinosLister = context.getBean(DinosLister.class);
+
+		if(dinosLister != null) {
+			System.out.println("That seems to be correctly initialized :)");
+		}
 	}
 
 }
